29edd7aa7fd40bf0c182576b388397b01e30ffec
aliasgeorge9329 <aliasgeorge9329@gmail.com>
2023-01-13 08:33:19 +0530

    mux 4x1

--- 4x1mux/mux_4x1_tb.v
+++ 4x1mux/mux_4x1_tb.v
@@ -15,11 +15,13 @@ module mux_4x1_tb();
 
       $monitor("T =%0t, i =%4b, s=%2b, out = %0b", $time, i, s, out);
 
-        s = 2'b00; #10;
-        s = 2'b01; #10;
-        s = 2'b10; #10;
-        s = 2'b11; #10;
-
+        s = 2'b00; #10
+        s = 2'b01; #10
+        s = 2'b10; #10
+        s = 2'b11; #10
+      
+      $dumpfile("mux_4x1.vcd");
+      $dumpvars(0, mux_4x1_tb);
       $finish;
 
     end
